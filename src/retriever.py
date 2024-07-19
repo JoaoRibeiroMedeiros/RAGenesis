f32b37270459e5cfaf1b8dbc19cced5486a08184
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-19 14:29:17 -0300

    reworking generation

--- src/retriever.py
+++ src/retriever.py
@@ -33,10 +33,9 @@ def encode(texts):
     return embeddings.cpu().numpy()
 
 
-gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
-
 def generate_response(query, retrieved_docs):
+    gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
     context = " ".join(retrieved_docs) + " " + query
     inputs = gpt2_tokenizer.encode(context, return_tensors="pt")
     outputs = gpt2_model.generate(inputs, max_length=100, num_return_sequences=1)
