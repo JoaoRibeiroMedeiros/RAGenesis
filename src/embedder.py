5006fcaeddd8c6ffde7a4c3da93d878622d3618a
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-19 18:56:53 -0300

    further modualrity and renaming

--- src/embedder.py
+++ src/embedder.py
@@ -4,11 +4,10 @@ import torch
 
 
 # Load a transformer model for embeddings
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-
 
 def encode(texts):
+    tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+    model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
     encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
     with torch.no_grad():
         model_output = model(**encoded_input)
--- src/retriever.py
+++ src/retriever.py
@@ -1,46 +1,12 @@
 import torch
 from transformers import AutoTokenizer, AutoModel, GPT2Tokenizer, GPT2LMHeadModel
+from chunker import chunk_bible
 import faiss
 import numpy as np
 import gradio as gr
 
 # Documents corpus (replace these with your actual documents)
 
-def chunk_bible(file_path):
-    verses = []
-
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Split the line at the first tab to separate the reference and the text
-            reference, text = line.split("\t", 1)
-            verses.append((reference.strip(), text.strip()))
-    
-    return verses
-
-documents = chunk_bible('sacred_data/bible.txt')
-
-
-# Load a transformer model for embeddings
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-
-
-def encode(texts):
-    encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-    embeddings = model_output.last_hidden_state.mean(dim=1)
-    return embeddings.cpu().numpy()
-
-
-def generate_response(query, retrieved_docs):
-    gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
-    context = " ".join(retrieved_docs) + " " + query
-    inputs = gpt2_tokenizer.encode(context, return_tensors="pt")
-    outputs = gpt2_model.generate(inputs, max_length=100, num_return_sequences=1)
-    response = gpt2_tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response
 
 def rag_system(query):
     documents = chunk_bible('sacred_data/bible.txt')
