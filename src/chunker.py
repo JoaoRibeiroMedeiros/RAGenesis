b371005e51738826d4e476ff1fabcd25b809f36e
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-27 03:49:16 -0300

    chunker fix

--- src/chunker.py
+++ src/chunker.py
@@ -1,13 +1,17 @@
 
-
+# %%
 
 def chunk_bible(file_path):
+
     verses = []
     with open(file_path, 'r') as file:
-        for line in file[1:]:
+        i=0
+        for line in file:
+            if i > 1:    
                 # Split the line at the first tab to separate the reference and the text
                 reference, text = line.split("\t", 1)
                 verses.append((reference.strip(), text.strip()))
+            i+=1
     return verses
 
-
+# %%
