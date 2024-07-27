3f0cd384daeeade950ab872dbb1a8c2aff14e825
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-27 04:18:58 -0300

    setting up embeddings to Milvus

--- src/vectordb2.py
+++ src/vectordb2.py
@@ -1,17 +1,51 @@
 
+# %%
+
 from pymilvus import connections, utility, FieldSchema, CollectionSchema, DataType, Collection
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from chunker import chunk_bible
+from src.chunker import chunk_bible
+import json
 
+# Read the config file
+with open('credentials/config.json', 'r') as file:
+    config = json.load(file)
+
+# Fetch the EC2 public IP
+ec2_public_ip = config['EC2_PUBLIC_IP']
+# %%
+
+
+# %%   
+
+# Load SentenceTransformer model
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+# Documents corpus (replace these with your actual documents)
+verses = chunk_bible('sacred_data/bible.txt')
+
+# Extracting just the text portions and references from each tuple
+verses_text = [verse[1] for verse in verses]
+verses_references = [verse[0] for verse in verses]
+# %%   
+
+# Generate embeddings
+embeddings = model.encode(verses_text)
+
+# Convert embeddings to list
+embeddings_list = embeddings.tolist()
+
+# %%
 
 # Connect to Milvus
-connections.connect(alias="default", host="localhost", port="19530")
+connections.connect(alias="default", host=ec2_public_ip, port="19530")
 
 # Define the schema for your collection
+# Define fields and schema for your collection
 fields = [
-    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),  # Notice auto_id=True
-    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=768)  # Change dim according to your embeddings
+    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name="reference", dtype=DataType.VARCHAR, max_length=100),
+    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=len(embeddings[0]))
 ]
 
 # Create a schema
@@ -24,28 +58,27 @@ if not utility.has_collection(collection_name):
 else:
     collection = Collection(name=collection_name)
 
-# Load SentenceTransformer model
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
-
-# Documents corpus (replace these with your actual documents)
-verses = chunk_bible('sacred_data/bible.txt')
-
-# Generate embeddings
-embeddings = model.encode(verses)
-
-# Convert embeddings to list
-embeddings_list = embeddings.tolist()
-
 # Prepare data for insertion
 # An empty list for IDs since auto_id=True
+# Insert data
 data = [
-    [],  # Empty as IDs will be auto-generated
-    embeddings_list  # List of embeddings
+    verses_references,  # List of references
+    embeddings          # List of embeddings
 ]
 
-# Insert data into collection
+# %%
+
 collection.insert(data)
+
+# Optionally: Load collection to memory (speeds up search)
+collection.load()
+# %%
+
+
+
+# %%
 
 # Check insertion
 print(f"Number of entities in Milvus: {collection.num_entities}")
+
+# %%
