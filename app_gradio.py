286c2d16e5d2d622816b1e8b501a64360e7abc99
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-29 17:26:17 -0300

    turn ip into local var

--- app_gradio.py
+++ app_gradio.py
@@ -5,18 +5,18 @@ from src.retriever import *
 
 # Documents corpus (replace these with your actual documents)
 
-with open('config.json', 'r') as file:
-    config = json.load(file)
-    # Fetch the EC2 public IP
-ec2_public_ip = config['EC2_PUBLIC_IP']
+def connect_and_query_holy_text(query):
 
-def query_holy_text_(query):
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+    # Fetch the EC2 public IP
+    ec2_public_ip = config['EC2_PUBLIC_IP']
     results_as_dicts = query_holy_text(query, ec2_public_ip)
     results_as_text = [result["reference"]+ ' - '+ result["verse"] for result in results_as_dicts]
     return results_as_text
 
 
-iface = gr.Interface(fn=query_holy_text_, 
+iface = gr.Interface(fn=connect_and_query_holy_text, 
                      inputs="text", 
                      outputs="text", 
                      title="Query the Bible verses!", 
