08d0b604167d8f7711831a2c009f0d7446f43e4f
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-07-30 19:44:54 -0300

    chunk quran

--- src/chunker.py
+++ src/chunker.py
@@ -15,3 +15,18 @@ def chunk_bible(file_path):
     return verses
 
 # %%
+
+
+def chunk_quran(file_path):
+    verses = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Split the line at the first tab to separate the reference and the text
+            print(line)
+            reference1, text = line.split("|", 1)
+            reference2, text = text.split("|", 1)
+            verses.append(('Surate '+ reference1+ ' verse ' + reference2, text.strip()))
+    return verses
+
+
+
