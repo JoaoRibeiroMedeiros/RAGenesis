b74c317ed72aaf55a9a3b62b523ecea0992a8e98
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-08-04 20:29:00 -0300

    fix schema field hierarchy

--- src/vectordb.py
+++ src/vectordb.py
@@ -4,23 +4,35 @@
 from pymilvus import connections, utility, FieldSchema, CollectionSchema, DataType, Collection
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from src.chunker import chunk_bible
+from src.chunker import chunk_bible, chunk_quran
 import json
 
-# Read the config file
-with open('config.json', 'r') as file:
-    config = json.load(file)
+# %%
 
-# Fetch the EC2 public IP
-ec2_public_ip = config['EC2_PUBLIC_IP']
+local= True
+
+if local == True:
+
+    host = 'localhost'
+
+else:
+    with open('config.json', 'r') as file: # Read the config file
+        config = json.load(file)
+    # Fetch the EC2 public IP
+    ec2_public_ip = config['EC2_PUBLIC_IP']
+    host = ec2_public_ip
 
 
 # Documents corpus (replace these with your actual documents)
-verses = chunk_bible('sacred_data/bible.txt')
+bible_verses = chunk_bible('sacred_data/bible.txt')
+quran_verses = chunk_quran('sacred_data/quran.txt')
 
 # Extracting just the text portions and references from each tuple
-verses_text = [verse[1] for verse in verses]
-verses_references = [verse[0] for verse in verses]
+bible_verses_text = [verse[1] for verse in bible_verses]
+bible_verses_references = [verse[0] for verse in bible_verses]
+
+quran_verses_text = [verse[1] for verse in quran_verses]
+quran_verses_references = [verse[0] for verse in quran_verses]
 
 # %%   
 
@@ -28,22 +40,23 @@ verses_references = [verse[0] for verse in verses]
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
 # Generate embeddings
-embeddings = model.encode(verses_text)
+
+bible_embeddings = model.encode(bible_verses_text)
+quran_embeddings = model.encode(quran_verses_text)
 
 # %%
 
 # Connect to Milvus
-connections.connect(alias="default", host=ec2_public_ip, port="19530")
+connections.connect(alias="default", host=host, port="19530")
 
-
-# %%
 # Define the schema for your collection
 # Define fields and schema for your collection
 fields = [
     FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name="holy-text", dtype=DataType.VARCHAR, max_length=40),
     FieldSchema(name="reference", dtype=DataType.VARCHAR, max_length=100),
-    FieldSchema(name="verse", dtype=DataType.VARCHAR, max_length=max([len(verse) for verse in verses_text])+5),
-    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=len(embeddings[0]))
+    FieldSchema(name="verse", dtype=DataType.VARCHAR, max_length=max([len(verse) for verse in bible_verses_text + quran_verses_text])+5),
+    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=len(bible_embeddings[0]))
 ]
 
 # %%
@@ -51,7 +64,6 @@ fields = [
 # Create a schema
 schema = CollectionSchema(fields, description="Collection for embeddings")
 
-# %%
 # Create a collection
 collection_name = "embeddings_collection"
 
@@ -66,14 +78,26 @@ collection = Collection(name=collection_name, schema=schema)
 
 # Insert data
 
-data = [
-    verses_references,  # List of references
-    verses_text,        # List of verses
-    [x for x in embeddings]          # List of embeddings
+bible_data = [
+    len(bible_verses_references) * ['Bible'],
+    bible_verses_references,  # List of references
+    bible_verses_text,        # List of verses
+    [x for x in bible_embeddings]          # List of embeddings
+]
+
+quran_data = [
+    len(bible_verses_references) * ['Quran'],
+    quran_verses_references,  # List of references
+    quran_verses_text,        # List of verses
+    [x for x in quran_embeddings]          # List of embeddings
 ]
 
 # %%
-collection.insert(data)
+
+collection.insert(bible_data)
+
+collection.insert(quran_data)
+
 # %%
 
 # Create an IVF_FLAT index for collection.
