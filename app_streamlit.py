a034e18925671b52b6d77421272b896a4f75b085
j.medeiros <joao_rmedeiros@yahoo.com.br>
2024-08-07 23:13:05 -0300

    last version

--- app_streamlit.py
+++ app_streamlit.py
@@ -10,88 +10,27 @@ from src.embedder import encode
 
 holy_texts = ['Bible', 'Quran', 'Gita', 'Analects']
 
+query = "God is love"
+
 local = True
 
 # %%
 
 def landing_page(): 
 
-    import streamlit as st
+    st.title('Holy AI!')
 
-st.title('Holy AI!')
+    st.markdown("""
 
-st.markdown("""
-Welcome to **Holy AI!**
+    Explore the holy texts of the **Bible**, the **Quran**, the **Bhagavad Gita**, and the **Analects** with the help of AI!
 
-Explore the holy texts of the **Bible**, the **Quran**, the **Bhagavad Gita**, and the **Analects** with the help of AI!
+    - **Exploration** : Explore the texts
+    - **VerseUniVerse** : Navigate through the verses: Click on a verse to find the most semantically similar verses in the selected holy texts.
+    - **RAGenesis** : Have a conversation with the oracle! Retrieval Augmented Genesis!
 
-Select the holy texts you want to explore and start your journey!
-
-There are three main features:
-
-- **Explore the texts**
-- **Navigate through the verses**: Click on a verse to find the most semantically similar verses in the selected holy texts.
-- **Have a conversation with the oracle!**
-
-The oracle will respond to you based on the toggled holy texts.
-
-**Enjoy your journey!**
-""")
-
-
-def exploration(): 
-
-    st.title('Explore Holy!')
-
-    st.text("Describe a subject or an idea you are interested in.")
-
-    st.text("AI will help you find the most relevant verses in the selected holy texts for it!")
-
-    query = st.text_input('Enter Query', value = 'God is love')
-
-    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, query, local=local)
-
-    for i, source, reference, verse in zip(range(len(results_verses)) ,results_sources,results_references, results_verses):
-        st.text(source)
-        st.button(reference, key=i, help=None, on_click=verse_uni_verse,   kwargs={"query_verse": verse})
-        st.text("")
-        st.text(verse)
-        st.text("")
-        
-
-def verse_uni_verse( query_verse = "In the beginning God created the heaven and the earth."):
-
-    st.title("Verse Uni Verse")
-
-    st.text("Navigate through the verses of the selected holy texts based on semantic similarity.")
-
-    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, query_verse, local=local)
-    
-    for i, source, reference, verse in zip(range(len(results_verses)), results_sources,results_references, results_verses):
-        st.text(source)
-        st.button(reference, key=i, help=None, on_click=verse_uni_verse,  kwargs={"query_verse": verse})
-        st.text("")
-        st.text(verse)
-        st.text("")
-        
-
-def genesis(): 
-
-    st.title('Retrieval Augmented Genesis!')
-
-    st.text("Speak to the oracle!")
-
-    st.text("Start a conversation! The oracle will respond to you based on the toggled holy texts.")
-
-    query = st.text_input('Speak to me...', value = 'God is love')
-
-    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, query, local=local)
-
-    retrieval = join_retrieved_references(results_references, results_verses)
-
-    response = get_oracle_response(query + retrieval)
-
-    st.text(response)
+    **Enjoy your journey!**            
+                
+    """)
 
 #### sidebar
 
@@ -102,6 +41,72 @@ selected_texts = st.sidebar.multiselect('Select Holy Texts', holy_texts, default
 # st.sidebar.button("RAGenesis", key=None, help=None, on_click=genesis)
 
 
+
+def exploration(): 
+
+    st.title('Explore Holy!')
+
+    st.markdown("Describe a subject or an idea you are interested in.")
+
+    st.markdown("AI will help you find the most relevant verses in the selected holy texts for it!")
+
+    query = st.text_input('Enter Query', value= query, on_change=exploration)
+
+    st.session_state.query = query
+
+    # if query:
+
+    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, st.session_state.query, local=local)
+
+    for i, source, reference, verse in zip(range(len(results_verses)) ,results_sources,results_references, results_verses):
+        st.markdown(source)
+        st.button(reference, key=i, help=None, on_click=verse_uni_verse,   kwargs={"query_verse": verse})
+        st.markdown("")
+        st.markdown(verse)
+        st.markdown("")
+    
+
+def verse_uni_verse( query_verse = "In the beginning God created the heaven and the earth."):
+
+    st.title("Verse Uni Verse")
+
+    st.markdown("Navigate through the verses of the selected holy texts based on semantic similarity.")
+
+    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, query_verse, local=local)
+
+    
+    for i, source, reference, verse in zip(range(len(results_verses)), results_sources,results_references, results_verses):
+        st.markdown(source)
+        st.button(reference, key=i, help=None, on_click=verse_uni_verse,  kwargs={"query_verse": verse})
+        st.markdown("")
+        st.markdown(verse)
+        st.markdown("")
+        
+
+def genesis(): 
+
+    st.title('Retrieval Augmented Genesis!')
+
+    st.markdown("Speak to the oracle!")
+
+    st.markdown("Start a conversation! The oracle will respond to you based on the toggled holy texts.")
+
+    query = st.text_input('Speak to me...', value= query, on_change=genesis)
+
+    # st.session_state.query = query
+
+    # if query:
+
+    results_sources, results_references, results_verses = connect_and_query_holy_text(selected_texts, query, local=local)
+
+    retrieval = join_retrieved_references(results_references, results_verses)
+
+    response = get_oracle_response(query + retrieval)
+
+    st.markdown(response)
+
+st.session_state.page = 'Landing'
+
 if st.sidebar.button("Exploration"):
     st.session_state.page = 'Exploration'
 if st.sidebar.button("VerseUniVerse"):
@@ -110,9 +115,6 @@ if st.sidebar.button("RAGenesis"):
     st.session_state.page = 'RAGenesis'
 
 # Main page logic
-
-st.session_state.page = 'Landing'
-
 
 if st.session_state.page == 'Landing':
     landing_page()
@@ -123,7 +125,6 @@ elif st.session_state.page == 'VerseUniVerse':
 elif st.session_state.page == 'RAGenesis':
     genesis()
 
+
+
 #### 
-
-
-
